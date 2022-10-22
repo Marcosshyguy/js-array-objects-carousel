e83343793eb4b7e56b924bed02661bc2b10b568a
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-10-22 18:27:58 +0200

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// DATA
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
+
+const sliderContainer = document.querySelector(".main-picture");
+
+const horizontalSliderContainer = document.querySelector(".miniatures-container");
+
+let thisObject
+// for( let imagesIndex = 0; imagesIndex < images.length; imagesIndex++){
+//     thisObject = images[imagesIndex]
+//     console.log(thisObject);
+// }
+
+images.forEach{(items)=>} 
+console.log(images)
